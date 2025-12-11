4618f859e8cac2b01cb70cca66d07d143bd1b7e7
unknown <bayuskakaja72@gmail.com>
2025-12-11 20:09:02 +0700

    perbaikan

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,19 +1,13 @@
 'use client';
 
 import { lusitana } from '@/app/ui/fonts';
-import {
-  ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { authenticate } from '@/app/lib/actions';
-import { useFormState } from 'react-dom';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
-
   return (
-    <form action={dispatch} className="space-y-3">
+    <form action={authenticate} method="post" className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Welcome to Dashboard
@@ -22,14 +16,6 @@ export default function LoginForm() {
         <Button className="mt-4 w-full" type="submit">
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )}
-        </div>
       </div>
     </form>
   );
