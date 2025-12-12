b5d135898f168e396c1c1b9a5d933d2ca3e26557
unknown <bayuskakaja72@gmail.com>
2025-12-12 19:10:42 +0700

    fixing for vercel

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -3,20 +3,19 @@
 import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
   return (
-    <form action={authenticate} method="post" className="space-y-3">
+    <div className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Welcome to Dashboard
         </h1>
         <p className="mb-6 text-sm text-gray-600">Click the button below to continue</p>
-        <Button className="mt-4 w-full" type="submit">
+        <Button className="mt-4 w-full" type="button">
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
       </div>
-    </form>
+    </div>
   );
 }
