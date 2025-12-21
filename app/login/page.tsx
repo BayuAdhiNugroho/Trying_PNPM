da3adb7bc677371d90259ab593c2999104000ec8
unknown <bayuskakaja72@gmail.com>
2025-12-21 15:29:01 +0700

    Finall

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Suspense } from 'react';
- 
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
@@ -11,7 +11,10 @@ export default function LoginPage() {
             <AcmeLogo />
           </div>
         </div>
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
+
       </div>
     </main>
   );
